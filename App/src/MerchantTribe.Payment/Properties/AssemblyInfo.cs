437aaaf0da36386c86b039c48501bdc4c8f4619b
Marcus McConnell <marcus@bvsoftware.com>
2012-07-17 14:50:38 -0400

    Updated version numbers for 1.4.1

--- App/src/MerchantTribe.Payment/Properties/AssemblyInfo.cs
+++ App/src/MerchantTribe.Payment/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using System;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.0.242")]
-[assembly: AssemblyFileVersion("1.4.0.242")]
+[assembly: AssemblyVersion("1.4.1.301")]
+[assembly: AssemblyFileVersion("1.4.1.301")]
 [assembly: CLSCompliant(true)]
 
 
